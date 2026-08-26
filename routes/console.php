addeeb698605ec208e760b3f9adc31d4a91e57b2
Angel Heriberto Rivera Saucedo <angelheriberto87@gmail.com>
2026-08-26 11:35:09 -0600

    [FIX] - Cron

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('class-sessions:auto-close')->everyFiveMinutes();
+Schedule::command('class-sessions:auto-close')->everyTwoMinutes();
